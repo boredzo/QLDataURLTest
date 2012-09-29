410f7f8083af178c8c1d50c6d8094c1cb93e8ec2
Peter Hosey <git@boredzo.org>
2012-09-29 12:30:38 -0700

    Create the image data URL and the image size in advance and stash them in properties, to be retrieved by the preview panel data source methods. Also use the scale factor properly when creating the thumbnail image.

--- QLDataURL/PRHThumbnailsWindowController.m
+++ QLDataURL/PRHThumbnailsWindowController.m
@@ -14,6 +14,9 @@
 
 @interface PRHThumbnailsWindowController ()
 
+@property NSSize imageSize;
+@property(copy) NSURL *imageDataURL;
+
 @end
 
 @implementation PRHThumbnailsWindowController
@@ -24,54 +27,52 @@
 
 - (id)initWithWindow:(NSWindow *)window
 {
-    self = [super initWithWindow:window];
-    if (self) {
-        // Initialization code here.
-    }
-    
-    return self;
+	self = [super initWithWindow:window];
+	if (self) {
+		NSSize size = { 128.0, 128.0 };
+		self.imageSize = size;
+
+		NSImage *sourceImage = [NSImage imageWithSize:size flipped:NO drawingHandler:^BOOL(NSRect dstRect) {
+			NSBezierPath *circle = [NSBezierPath bezierPathWithOvalInRect:(NSRect){ NSZeroPoint, self.imageSize }];
+			[[NSColor redColor] set];
+			[circle fill];
+			return YES;
+		}];
+		NSData *sourceData = [sourceImage TIFFRepresentation];
+		self.imageDataURL = [sourceData dataURLWithMimeType_PRH:@"image/tiff"];
+	}
+	
+	return self;
 }
 
 - (void)windowDidLoad {
     [super windowDidLoad];
 
-	NSSize size = { 128.0, 128.0 };
-	CGFloat scaleFactor = self.window.userSpaceScaleFactor;
-	size.width *= scaleFactor;
-	size.height *= scaleFactor;
-
-	NSImage *sourceImage = [NSImage imageWithSize:size flipped:NO drawingHandler:^BOOL(NSRect dstRect) {
-		NSBezierPath *circle = [NSBezierPath bezierPathWithOvalInRect:(NSRect){ NSZeroPoint, size }];
-		[[NSColor redColor] set];
-		[circle fill];
-		return YES;
-	}];
-	NSData *sourceData = [sourceImage TIFFRepresentation];
-	NSURL *dataURL = [sourceData dataURLWithMimeType_PRH:@"image/tiff"];
+	NSDictionary *options = @{ (__bridge NSString *)kQLThumbnailOptionScaleFactorKey: @(self.window.userSpaceScaleFactor) };
 
 	//Try to generate thumbnails for said URL
-	CGImageRef thumbnailCGImage = QLThumbnailImageCreate(kCFAllocatorDefault, (__bridge CFURLRef)dataURL, size, /*options*/ NULL);
+	CGImageRef thumbnailCGImage = QLThumbnailImageCreate(kCFAllocatorDefault, (__bridge CFURLRef)self.imageDataURL, self.imageSize, (__bridge CFDictionaryRef)options);
 	if (thumbnailCGImage) {
-		self.synchronouslyObtainedThumbnailImage = [[NSImage alloc] initWithCGImage:thumbnailCGImage size:size];
+		self.synchronouslyObtainedThumbnailImage = [[NSImage alloc] initWithCGImage:thumbnailCGImage size:self.imageSize];
 		CFRelease(thumbnailCGImage);
 	}
 
-	QLThumbnailRef thumbnailAttempt = QLThumbnailCreate(kCFAllocatorDefault, (__bridge CFURLRef)dataURL, size, /*options*/ NULL);
+	QLThumbnailRef thumbnailAttempt = QLThumbnailCreate(kCFAllocatorDefault, (__bridge CFURLRef)self.imageDataURL, self.imageSize, (__bridge CFDictionaryRef)options);
 	if (thumbnailAttempt) {
 		thumbnailCGImage = QLThumbnailCopyImage(thumbnailAttempt);
 		CFRelease(thumbnailAttempt);
 	}
 	if (thumbnailCGImage) {
-		self.dispatchObtainedThumbnailImage	= [[NSImage alloc] initWithCGImage:thumbnailCGImage size:size];
+		self.dispatchObtainedThumbnailImage = [[NSImage alloc] initWithCGImage:thumbnailCGImage size:self.imageSize];
 		CFRelease(thumbnailCGImage);
 	}
 
-	thumbnailAttempt = QLThumbnailCreate(kCFAllocatorDefault, (__bridge CFURLRef)dataURL, size, /*options*/ NULL);
+	thumbnailAttempt = QLThumbnailCreate(kCFAllocatorDefault, (__bridge CFURLRef)self.imageDataURL, self.imageSize, (__bridge CFDictionaryRef)options);
 	if (thumbnailAttempt) {
 		QLThumbnailDispatchAsync(thumbnailAttempt, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, /*flags*/ 0), ^{
 			CGImageRef resultCGImage = QLThumbnailCopyImage(thumbnailAttempt);
 			if (resultCGImage) {
-				NSImage *resultImage = [[NSImage alloc] initWithCGImage:resultCGImage size:size];
+				NSImage *resultImage = [[NSImage alloc] initWithCGImage:resultCGImage size:self.imageSize];
 				dispatch_async(dispatch_get_main_queue(), ^{
 					self.dispatchObtainedThumbnailImage = resultImage;
 				});
